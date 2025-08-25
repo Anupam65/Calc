586e9196468067a1f273c40c80f01bb638e3ddf8
Anupam65 <anupam9866@gmail.com>
2025-08-25 10:30:58 +0530

    new

--- src/api.js
+++ src/api.js
@@ -1,14 +1,13 @@
-export const BASEURL = "http://localhost:8080/calculator/";
-export function callApi(reqmethod, url, data, responseHandler)
-{
+export const BASEURL = "http://localhost:8090/";
+export function callApi(reqmethod, url, data, responseHandler) {
     let options = "";
-    if(reqmethod === "GET" || reqmethod === "DELETE")
-        options = {method: reqmethod, headers:{'Content-Type':'application/json'}};
+    if (reqmethod === "GET" || reqmethod === "DELETE")
+        options = { method: reqmethod, headers: { 'Content-Type': 'application/json' } };
     else
-        options = {method: reqmethod, headers:{'Content-Type':'application/json'}, body: data};
+        options = { method: reqmethod, headers: { 'Content-Type': 'application/json' }, body: data };
     fetch(url, options)
         .then(response => {
-            if(!response.ok)
+            if (!response.ok)
                 throw new Error(response.status + ": " + response.statusText);
             return response.text();
         })
